22c2b6510119e17e3d01fee0603cbbf2b1290215
VeqsaGit <77352090+VeqsaGit@users.noreply.github.com>
2021-01-17 11:02:56 +0100

    update
    koel

--- app.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-
-fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h')
-  .then(response => response.json())
-  .then(function(data) {
-      console.log(JSON.stringify(data[0]))
-})
